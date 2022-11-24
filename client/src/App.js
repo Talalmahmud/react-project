62c466f05125543e1ad0334645c8715ed89f8d65
Talal Mahmood <talalmahmud2011@gmail.com>
2022-11-24 10:34:31 +0600

    ecommarce project

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">tradeOf</a>
+      </header>
+      <main>
+        product list
+      </main>
+    </div>
+  );
+}
+
+export default App;
